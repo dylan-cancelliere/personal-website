6a2b9891e2ecc3b58c9aa2d6c47cb655a188761e
Dylan Cancelliere <dycancel@gmail.com>
2025-11-02 09:51:35 -0500

    october playlist

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -102,7 +102,7 @@ const SpotifyContainer = () => {
             maxWidth: rem(700),
             flexGrow: 1,
           }}
-          src="https://open.spotify.com/embed/playlist/3wFm5kys2BGoN8Wxrgn8Jj?utm_source=generator"
+          src="https://open.spotify.com/embed/playlist/6U4twRNeuWDpzCs0zOvpLl?utm_source=generator"
           height="352"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
